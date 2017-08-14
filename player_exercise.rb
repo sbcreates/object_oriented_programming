505d18a087a0f113c25506dc4d47b36f1bc6192d
sbcreates <drawsarahb@gmail.com>
2017-08-14 16:34:18 -0400

    Tested level_up method and works

--- player_exercise.rb
+++ player_exercise.rb
@@ -1,9 +1,46 @@
 class Player
 
-  def initialize(gold_coins, health_points, lives)
-    @gold_coins = gold_coins
-    @health_points = health_points
-    @lives = lives
+  def initialize
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
   end
 
-  
+  def level_up
+    @lives += 1
+    "Congrats! You've just earned a point. You now have #{@lives} lives"
+  end
+
+  def collect_treasure
+    if @gold_coins % 10
+      @lives.level_up && @gold_coins += 1
+      "Awesome! You just earned a bonus life. You now have #{@gold_coins} gold coins and #{@lives} lives"
+    else
+      @gold_coins += 1
+      "You now have #{@gold_coins} gold coins"
+    end
+  end
+
+  def do_battle(damage)
+    if @health_points < 1
+      @lives -= 1 && @health_points = 10
+      "You've lost a life. You now have #{@lives} lives and your health is #{@health_points}"
+    elsif @lives == 0
+      @gold_coins.restart && @health_points.restart && @lives.restart
+      "You're all out of lives. Time to restart. Resetting lives to #{@lives}, gold coins to #{@gold_coins}, and health to #{@health_points}."
+    else
+      @health_points -= damage
+      "You've lost some health. Your health is #{@health_points}"
+    end
+  end
+
+  def restart
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+  end
+end
+
+player_one = Player.new
+
+puts player_one.level_up
