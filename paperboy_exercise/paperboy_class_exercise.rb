678f4833ee68ef2852b94bf0565731cf9de7a79d
sbcreates <drawsarahb@gmail.com>
2017-08-14 23:02:24 -0400

    Completed Paperboy exercise.

--- paperboy_exercise/paperboy_class_exercise.rb
+++ paperboy_exercise/paperboy_class_exercise.rb
@@ -7,26 +7,31 @@ class Paperboy
   end
 
   def quota
-    if @experience == 0
-      @experience = 50
+    if @experience < 1
+      @todays_quota = 50
     else
-      @experience += (@experience / 2) + 50
+      @todays_quota = (@experience / 2) + 50
     end
   end
 
   def deliver(start_address, end_address)
-    delivered = end_address + start_address + 1
-    if delivered == @experience
-      ( delivered * 0.25 )
-    elsif delivered > @experience
-      (( delivered - @experience ) * 0.50 ) + ( @experience * 0.25 )
+    @delivered = (end_address - start_address) + 1
+    @experience += @delivered
+    if @delivered == @todays_quota
+      @todays_earnings = (@delivered * 0.25)
+    elsif @delivered > @todays_quota
+      @todays_earnings = ((@delivered - @todays_quota) * 0.50) + (@todays_quota * 0.25)
     else
-      ( delivered * 0.25 ) - 2
+      @todays_earnings = ( @delivered * 0.25 ) - 2.0
     end
   end
 
+  def earnings
+    @earnings += @todays_earnings
+  end
+
   def report
-    "I'm #{@name} and I delivered #{@experience} papers and I've earned #{@earnings}"
+    "I'm #{@name} and I've delivered #{@experience} papers today and I've earned $#{@earnings}."
   end
 
 end
@@ -35,10 +40,15 @@ chuck = Paperboy.new("Chuck", 0, 0)
 
 puts chuck.quota
 puts chuck.deliver(101, 160)
-# puts chuck.earnings
+puts chuck.earnings
 puts chuck.report
 
 puts chuck.quota
 puts chuck.deliver(1, 75)
-# puts chuck.earnings
+puts chuck.earnings
+puts chuck.report
+
+puts chuck.quota
+puts chuck.deliver(61, 200)
+puts chuck.earnings
 puts chuck.report
